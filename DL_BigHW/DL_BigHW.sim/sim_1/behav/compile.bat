5fe58f0b23b21fab1d57e7f49e241f63e9d2a076
NatsuIce <wangxilin@tongji.edu.cn>
2024-12-08 00:24:54 +0800

    edit sync files

--- DL_BigHW/DL_BigHW.sim/sim_1/behav/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set bin_path=C:\\WorkSpace\\ModelSim\\win32pe
-call %bin_path%/vsim  -c -do "do {sel_display7_tb_compile.do}" -l compile.log
-if "%errorlevel%"=="1" goto END
-if "%errorlevel%"=="0" goto SUCCESS
-:END
-exit 1
-:SUCCESS
-exit 0
